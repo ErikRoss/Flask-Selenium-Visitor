32026cf8262e74bf84716e147c6261d107eb3b29
ErikRoss <37239211+ErikRoss@users.noreply.github.com>
2023-03-12 17:55:14 +0200

    Update page_visitor.py

--- app/page_visitor.py
+++ app/page_visitor.py
@@ -1,14 +1,12 @@
 import json
 import logging
 import time
+import traceback
 
-# import pyvirtualdisplay
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
-# from browsermobproxy import Server
 
 
 class Browser:
@@ -33,7 +31,6 @@ class Browser:
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--remote-debugging-port=9222')
-        # options.add_argument('start-maximized')
         options.add_argument('disable-infobars')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-extensions')
@@ -48,24 +45,10 @@ class Browser:
         options.add_argument('--disable-breakpad')
         options.add_experimental_option("excludeSwitches", ['enable-automation'])
         options.add_argument(f'user-agent={useragent}')
-        # options.add_argument("--proxy-server={0}".format(self.start_proxy_server().proxy))
         service = ChromeService(executable_path=ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
 
         return driver
-
-    # def start_proxy_server(self):
-    #     # self.server = Server(path="/var/www/webservice/app/bmp/bin/browsermob-proxy")
-    #     self.server = Server(path="D:/Work/Fingerprint_handler/app/bmp/bin/browsermob-proxy.bat")
-    #     self.server.start()
-    #     proxy = self.server.create_proxy()
-    #     headers = {
-    #         "X-Device-Model": "Poco F3",
-    #         "X-Operating-System": "Android",
-    #         "X-Operating-System-Version": "12"
-    #     }
-    #     proxy.headers(headers)
-    #     return proxy
 
     @staticmethod
     def set_cookies(driver, cookies_data):
@@ -92,38 +75,45 @@ class Browser:
         :return: True - посещение успешно выполнено, False - не удалось посетить страницу
         """
         visited = False
+        logging.info(f"Got request with vmc: {vmc} and xcn: {xcn}")
+        driver = self.start_driver(user_data.useragent)
+        logging.info(f"Browser started")
+        try:
+            driver.get(f"https://{user_data.domain}")
+            logging.info(f"Visited main page")
+        except Exception:
+            logging.critical(f"Got error visiting Main page: {traceback.format_exc()}")
+            return False
+        url = f"https://{user_data.domain}/MXpPH5?ocs=Bu001xPA20&rma={user_data.rma}&vmc={vmc}" \
+              f"&fbclid={user_data.fbclid}&xcn={xcn}&_token={user_data.token}"
         for i in range(5):
             try:
-                logging.info(f"Got request with vmc: {vmc} and xcn: {xcn}")
-                driver = self.start_driver(user_data.useragent)
-                logging.info(f"Browser started")
-                print("Browser started")
-                # driver.set_window_size(user_data.screen_width, user_data.screen_height)
-                # logging.info(f"Browser window size set to {user_data.screen_width}x{user_data.screen_height}")
-                driver.get(f"https://{user_data.domain}")
-                logging.info(f"Visited main page")
-                print("Opened main page")
                 self.set_cookies(driver, user_data.cookies)
                 logging.info(f"Cookies successfully set")
-                print("Cookies set")
-                url = f"https://{user_data.domain}/MXpPH5?ocs=Bu001xPA20&rma={user_data.rma}&vmc={vmc}" \
-                      f"&fbclid={user_data.fbclid}&xcn={xcn}&_token={user_data.token}"
+            except Exception:
+                logging.critical(f"Got error setting cookies: {traceback.format_exc()}")
+                time.sleep(3)
+                continue
+            try:
                 driver.get(url)
                 logging.info(f"Target page visited: {url}")
                 print("Target page visited")
+            except WebDriverException:
+                visited = False
+                logging.error(f"Error starting Chrome browser: {traceback.format_exc()}")
+                print("Error starting Chrome browser")
+            else:
                 time.sleep(15)
                 driver.quit()
                 logging.info(f"Browser closed")
                 print("Browser closed")
-                # self.server.stop()
-            except WebDriverException:
-                visited = False
-                logging.error(f"Error starting Chrome browser")
-                print("Error starting Chrome browser")
-            else:
                 visited = True
                 break
-
+        if visited is False:
+            try:
+                driver.quit()
+            except Exception:
+                pass
         return visited
 
 
